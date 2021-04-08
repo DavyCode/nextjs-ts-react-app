0310ab6c177c0e36123ca256d6d1518dad2ed413
Paul Azemoh <pdaswift3009@gmail.com>
2021-04-08 23:13:35 +0100

    initial commit

---
+++ pages/api/hello.ts
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Data } from '../../types';
+
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.status(200).json({ name: 'John Doe' })
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+function HomePage() {
+  return <div>Welcome to Next.js React TS app!!!!</div>
+}
+
+export default HomePage
